03e3a499f2305f84da48b902c22951d2ba45a53c
NicholasYerolemou <yerolemou.nicholas@gmail.com>
2023-09-20 15:15:20 +0200

    add TCP code in server and client, can now send messages to eachother

--- Client.java
+++ Client.java
@@ -1,135 +1,72 @@
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.cert.X509Certificate;
-import java.security.cert.X509Extension;
-import java.security.cert.CertificateExpiredException;
-import java.security.cert.CertificateNotYetValidException;
-import java.security.cert.CertificateException;
-import java.security.cert.CertificateParsingException;
-import java.util.List;
-import java.util.Date;
-
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Client {
-
-  private PublicKey publicKey;
-  private PrivateKey privateKey;
-  private SecretKey sessionKey;
-  private X509Certificate certificate = null;
-  private static Client clientInstance = null;
+  public static final String SERVER_HOST = "localhost"; // Replace with the server's hostname or IP address
+  public static final int SERVER_PORT = 12345;
 
   public static void main(String[] args) {
-
-    clientInstance = new Client();
-    clientInstance.initializeClient();
-  }
-
-  public void startClient(Client client) {// used by the test class to pass an instance of this class to the methods
-                                          // here
-    System.out.println("Client started");
-    clientInstance = client;
-    clientInstance.initializeClient();
-  }
-
-  private void initializeClient() {
-    clientInstance.genClientKeys();
-    certificate = CA.signUp();
-    if (certificate == null) {
-      System.out.println("Error: Certificate is Null");
-    } else {
-      System.out.println("Certificate recieved");
-      clientInstance.verifyCertificate(certificate);
-    }
-
-  }
-
-  private void establishSession(boolean source) {
-    // if source, creates a session, if not, completes the session the other client
-    // initiated
-    if (source) {// this client is beginning the session
-
-      // send your public key
-      // get their public key
-      // apprach CA for their certificate
-      // compare their certificate and public key
-
-      clientInstance.genSessionKey();
-      // encrypt session key using their pub key
-      // encrypt session key using our private key
-      // share session key using their public key
-
-    } else {// this client is NOT beginning the session
-
-      // return your public key
-      // get public key of sender
-      // get certificate of sender
-      // compare public key and certificate
-      // recieve the session key
-      // decrypt session key using private key
-      // decrpty session key using their public key
-    }
-
-  }
-
-  private void genClientKeys() {// generate the public and private keys for this client
-
     try {
-      // Generate a key pair using RSA
-      KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-      keyPairGenerator.initialize(2048);
+      Socket socket = new Socket(SERVER_HOST, SERVER_PORT);
+      System.out.println("Connected to the server.");
 
-      // Generate the key pair
-      KeyPair keyPair = keyPairGenerator.generateKeyPair();
+      // waits for input from the user
+      OutputStream output = socket.getOutputStream();
+      sendMessage("hello", output);
 
-      // Retrieve the public and private keys from the key pair
-      publicKey = keyPair.getPublic();
-      privateKey = keyPair.getPrivate();
-      System.out.println("Public and Private keys generated");
+      // waits for a message from the server
+      BufferedReader serverListener = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      Thread responseThread = new Thread(() -> {
+        try {
+          while (true) {
+            String serverMessage = serverListener.readLine();
+            if (serverMessage == null) {
+              // Server closed the connection
+              System.out.println("Server disconnected.");
+              disconnect(socket);
+              break;
+            }
+            // we have recieved a message from the server, process that message
+            System.out.println("Message recieved");
+            receiveMessage(serverMessage);
+          }
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      });
+      responseThread.start();
 
-    } catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
-    }
-
-  }
-
-  private void genSessionKey() {
-    try {
-      // Create a KeyGenerator for AES
-      KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-      int keySize = 128;
-      keyGen.init(keySize);
-      // Generate the AES session key
-      sessionKey = keyGen.generateKey();
-      System.out.println("Session key generated");
-
-    } catch (NoSuchAlgorithmException e) {
+    } catch (IOException e) {
+      System.out.println("Failed to connect to server");
       e.printStackTrace();
     }
   }
 
-  private boolean verifyCertificate(X509Certificate cert) {
-
+  private static void sendMessage(String message, OutputStream output) {
     try {
-      // Check if the certificate is not expired
-      cert.checkValidity();
-
-      System.out.println("The certificate is legit");
-      return true;
-
-    } catch (CertificateExpiredException | CertificateNotYetValidException e) {
-      System.out.println("The certificate is not legit");
-      return false;
+      byte[] msg = (message + "\n").getBytes();
+      output.write(msg);
+      System.out.println("Sent message");
+    } catch (IOException e) {
+      System.out.println("Failed to send message");
+      e.printStackTrace();
     }
+  }
 
+  private static void receiveMessage(String msg) {
+
+    System.out.println("Message recieved: " + msg);
+
+  }
+
+  private static void disconnect(Socket socket) {
+    try {
+      socket.close();
+    } catch (IOException e) {
+      System.out.println("Disconnected from server");
+      e.printStackTrace();
+    }
   }
 
 }
-
-// get certificate from CA
-// get public key from other user
-// compare the two
--- Server.java
+++ Server.java
@@ -1,6 +1,62 @@
-public class Server {
-  public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+  public static final int PORT = 12345; // Change this to the desired port number
+
+  public static void main(String[] args) {
+    try {
+      ServerSocket serverSocket = new ServerSocket(PORT);
+      System.out.println("Server is listening on port " + PORT);
+
+      // Accept incoming client connections
+      while (true) {
+        Socket clientSocket = serverSocket.accept();
+        System.out.println("Client connected from " + clientSocket.getInetAddress().getHostAddress());
+
+        // Create a separate thread to handle the client connection
+        Thread clientThread = new Thread(new ClientHandler(clientSocket));
+        clientThread.start();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  // Define a separate class for handling client connections
+  static class ClientHandler implements Runnable {
+    private Socket clientSocket;
+
+    public ClientHandler(Socket socket) {
+      this.clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+      try {
+        // Handle the client connection here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);// output stream to write data to the
+                                                                                   // client.
+
+        String clientMessage;
+        while ((clientMessage = reader.readLine()) != null) {
+          // Handle client requests and send responses
+          System.out.println("Received from client: " + clientMessage);
+          writer.println("OK");
+        }
+
+        // Close the client socket when done
+        clientSocket.close();
+        System.out.println("Client disconnected.");
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
   }
 
 }
